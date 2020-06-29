5d71a3de36c56e28f15ad9c2be893997a05ff8f1
Luiz Chaves <lucachaves@gmail.com>
2020-06-29 03:19:17 -0300

    buscando poviders da API

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -46,6 +46,8 @@ const AuthProvider: React.FC = ({ children }) => {
       ]);
 
       if (token[1] && user[1]) {
+        api.defaults.headers.authorization = `Bearer ${token[1]}`;
+
         setData({ token: token[1], user: JSON.parse(user[1]) });
       }
 
@@ -67,6 +69,8 @@ const AuthProvider: React.FC = ({ children }) => {
       ['@GoBarber:user', JSON.stringify(user)],
       ['@GoBarber:token', token],
     ]);
+
+    api.defaults.headers.authorization = `Bearer ${token}`;
 
     setData({ token, user });
   }, []);
